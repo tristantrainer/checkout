fccc3609cf4c06e293bdadac7df848ced3621251
tristan.trainer <tristantrainer.su@gmail.com>
2024-08-21 23:15:07 +0100

    feat: implemented dictionary extension AddOrUpdate

--- src/BrightHR.Checkout.Application/Extensions/DictionaryExtensions.cs
+++ src/BrightHR.Checkout.Application/Extensions/DictionaryExtensions.cs
@@ -5,6 +5,6 @@ internal static class DictionaryExtensions
     public static void AddOrUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue newValue, Func<TValue, TValue> updater)
         where TKey : notnull
     {
-        dictionary[key] = newValue;
+        dictionary[key] = dictionary.TryGetValue(key, out TValue? existing) ? updater(existing) : newValue;
     }
 }
