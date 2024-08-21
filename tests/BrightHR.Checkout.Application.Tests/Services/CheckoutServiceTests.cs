35be71351b4eaab730aa7914a045d8c1b2550f3e
tristan.trainer <tristantrainer.su@gmail.com>
2024-08-21 13:14:30 +0100

    wip: red test second requirement

--- tests/BrightHR.Checkout.Application.Tests/Services/CheckoutServiceTests.cs
+++ tests/BrightHR.Checkout.Application.Tests/Services/CheckoutServiceTests.cs
@@ -20,5 +20,21 @@ public class CheckoutServiceTests
         Assert.Equal(expected, actual);
     }
 
+    [Fact]
+    public void GetTotalPrice_WhenItemScanned_ReturnsItemPrice() 
+    {
+        // Arrange
+        var itemPrice = 123;
+
+        var checkout = new CheckoutService();
+        var expected = itemPrice;
+
+        // Act
+        var actual = checkout.GetTotalPrice();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
     #endregion GetTotalPrice
 }
