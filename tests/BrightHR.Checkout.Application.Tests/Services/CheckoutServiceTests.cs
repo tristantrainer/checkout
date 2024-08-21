c3b1ba74ef58602b88fc5e284eba728f8bf46d8b
tristan.trainer <tristantrainer.su@gmail.com>
2024-08-21 13:49:02 +0100

    wip: red test third requirement

--- tests/BrightHR.Checkout.Application.Tests/Services/CheckoutServiceTests.cs
+++ tests/BrightHR.Checkout.Application.Tests/Services/CheckoutServiceTests.cs
@@ -45,5 +45,34 @@ public class CheckoutServiceTests
         Assert.Equal(expected, actual);
     }
 
+    [Fact]
+    public void GetTotalPrice_WhenMultipleItemScanned_ReturnsSumOfUnitPrices() 
+    {
+        // Arrange
+        var unitPriceRepositoryMock = new Mock<IUnitPriceRepository>();
+        unitPriceRepositoryMock
+            .Setup((repo) => repo.GetUnitPrice("A"))
+            .Returns(1);
+        unitPriceRepositoryMock
+            .Setup((repo) => repo.GetUnitPrice("B"))
+            .Returns(20);
+        unitPriceRepositoryMock
+            .Setup((repo) => repo.GetUnitPrice("C"))
+            .Returns(300);
+
+        var checkout = new CheckoutService(unitPriceRepositoryMock.Object);
+        var expected = 321;
+
+        checkout.Scan("A");
+        checkout.Scan("B");
+        checkout.Scan("C");
+
+        // Act
+        var actual = checkout.GetTotalPrice();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
     #endregion GetTotalPrice
 }
